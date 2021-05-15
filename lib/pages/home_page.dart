58c565b5bc098c5d452fa1ba53d345c222b75960
alfianardhi <ardhi.alfian@gmail.com>
2021-05-16 00:31:47 +0700

    initiate

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text(
+        'Chat Home',
+        style: TextStyle(
+          fontSize: 35,
+        ),
+        ),
+    );
+  }
+}
