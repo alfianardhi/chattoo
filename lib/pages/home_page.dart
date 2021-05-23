ccdd8dd2c41695f44ef204fcd4a2fdf4f00ad54c
alfianardhi <ardhi.alfian@gmail.com>
2021-05-23 22:48:02 +0700

    inprog

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:chattoo/theme.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Image.asset('assets/images/profile1.png'),
+      body: Container(
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 46,
+            ),
+            Text(
+              'Chat',
+              style: TextStyle(
+                fontSize: 48,
+                color: blackColor,
+                fontWeight: FontWeight.w400,
+              )),
+          ],
+        ),
+      ),
     );
   }
 }
